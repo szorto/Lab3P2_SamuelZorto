b86ec382c4f87ddb9f58af99de731a6e29a3e772
szorto <samuelzorto@gmail.com>
2023-02-03 13:12:26 -0600

    Primer commit, comenzando el lab

---
+++ src/lab3p2_samuelzorto/Lab3P2_SamuelZorto.java
@@ -0,0 +1,12 @@
+package lab3p2_samuelzorto;
+
+public class Lab3P2_SamuelZorto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // ;/
+    }
+    
+}
